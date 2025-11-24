0f7757f83102e0a5ebd56dc45a1fc2d4bfd31a18
Viwu <sinconsistencia@gmail.com>
2025-11-23 22:25:54 -0600

    mainmenu stuff ill have to work on

--- source/sphis/poan/InitState.hx
+++ source/sphis/poan/InitState.hx
@@ -1,5 +1,7 @@
 package sphis.poan;
 
+import sphis.poan.states.MainMenuState;
+import flixel.FlxG;
 import flixel.FlxState;
 
 class InitState extends FlxState
@@ -7,6 +9,8 @@ class InitState extends FlxState
 	override public function create()
 	{
 		super.create();
+
+		FlxG.switchState(() -> new MainMenuState());
 	}
 
 	override public function update(elapsed:Float)
---
+++ source/sphis/poan/states/MainMenuState.hx
@@ -0,0 +1,16 @@
+package sphis.poan.states;
+
+import flixel.FlxState;
+
+class MainMenuState extends FlxState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
